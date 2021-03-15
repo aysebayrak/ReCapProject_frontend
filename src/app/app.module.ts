47775bef3fa66d496688788e4169aceeb7f35c69
Ayşe BAYRAK <202803003@ogr.cbu.edu.tr>
2021-03-15 11:38:59 +0300

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { CarComponent } from './components/car/car.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { RentalComponent } from './components/rental/rental.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandComponent,
+    CarComponent,
+    ColorComponent,
+    CustomerComponent,
+    NaviComponent,
+    RentalComponent
   ],
   imports: [
     BrowserModule,
