8ba5960d26f47282e61569c43cd1b7739297e519
Ayşe BAYRAK <202803003@ogr.cbu.edu.tr>
2021-03-29 17:26:38 +0300

    .

--- src/app/components/car-add/car-add.component.ts
+++ src/app/components/car-add/car-add.component.ts
@@ -30,21 +30,24 @@ export class CarAddComponent implements OnInit {
   add(){
     if(this.carAddForm.valid){
       let carModel=Object.assign({},this.carAddForm.value)
-      this.carService.add(carModel).subscribe(data=>{
-        console.log(data)
-        this.toastrService.success(data.message, "Başarılı")
+      this.carService.add(carModel).subscribe(response=>{
+        this.toastrService.success(response.message, "Başarılı")
 
       },responseError=>{
-        console.log(responseError.error)
-        this.toastrService.error(responseError.error)
+        if(responseError.error.Errors.length>0){
+          for (let i = 0; i < responseError.error.Errors.length; i++) {
+            
+            this.toastrService.error(responseError.error.Errors[i].ErrorMessage,"Doğrulama Hatası")
+          }
+          
+        }
+        
       })
     }
     else{
       this.toastrService.error("Form Bilgilerini Eksiksiz Doldurunuz...","Dikkat")
 
     }
-    
-    
     
   }
 
